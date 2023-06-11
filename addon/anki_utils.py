49f10b03c0cd9496f39bc1d49ca51b9e754794bb
Luc W <lucw@airpost.net>
2023-06-11 18:39:02 +0800

    implement methods in anki_utils

--- addon/anki_utils.py
+++ addon/anki_utils.py
@@ -7,10 +7,11 @@ class AnkiUtils():
         pass
 
     def get_note_type_list(self) -> List[str]:
-        return []
+        return aqt.mw.col.models.all_names()
 
     def get_field_list_for_note_type(self, note_type_name: str) -> List[str]:
-        return []
+        model = aqt.mw.col.models.by_name(note_type_name)
+        return aqt.mw.col.models.field_names(model)
 
     def get_deck_list(self) -> List[str]:
-        return []
+        return aqt.mw.col.decks.all_names()
