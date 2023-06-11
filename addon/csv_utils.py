88012bf175f8af025ae595269f16f6fc8361dcc8
Luc W <lucw@airpost.net>
2023-06-11 22:07:12 +0800

    open files using utf-8 encoding

--- addon/csv_utils.py
+++ addon/csv_utils.py
@@ -2,24 +2,24 @@ import csv
 import tempfile
 
 def create_csv_without_header(csv_file_path):
-    with open(csv_file_path, newline='') as csvfile:
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
-        with tempfile.NamedTemporaryFile(mode='w', delete=False, newline='') as temp_file:
+        with tempfile.NamedTemporaryFile(mode='w', delete=False, newline='', encoding='utf-8') as temp_file:
             writer = csv.writer(temp_file)
             for row in reader:
                 writer.writerow(row)
             return temp_file
 
 def get_fieldnames(csv_file_path):
-    with open(csv_file_path, newline='') as csvfile:
+    with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         header = next(reader)
         return header
 
 def get_fieldname_to_index(csv_file_path: str):
     # get the list of fields in the csv_file_path CSV file
-    with open(csv_file_path, 'r') as csv_file:
+    with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
         csv_header = csv_file.readline()
         csv_fields = csv_header.split(',')
         csv_fields = [x.strip() for x in csv_fields]
