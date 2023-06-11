25833ccef52132492c7c36151ece197dd7be984c
Luc W <lucw@airpost.net>
2023-06-11 16:31:44 +0800

    check setting note type

--- addon/gui.py
+++ addon/gui.py
@@ -1,10 +1,15 @@
 from PyQt6.QtWidgets import QDialog, QLabel, QComboBox, QPushButton, QVBoxLayout
+from typing import List
 from . import anki_utils
 from . import data
 
 class ConfigureTableImportDialog(QDialog):
-    def __init__(self, model: data.TableImportConfig, anki_utils, parent=None):
+    UNMAPPED_FIELD_NAME = '(Unmapped)'
+
+    def __init__(self, model: data.TableImportConfig, csv_field_names: List[str], anki_utils, parent=None):
         super().__init__(parent)
+        self.csv_field_names = csv_field_names
+        self.all_field_names = [self.UNMAPPED_FIELD_NAME] + csv_field_names
         self.anki_utils = anki_utils
         self.setWindowTitle("Anki Import")
         self.note_type_label = QLabel("Note Type:")
@@ -19,6 +24,11 @@ class ConfigureTableImportDialog(QDialog):
         layout.addWidget(self.deck_label)
         layout.addWidget(self.deck_combo)
         layout.addWidget(self.ok_button)
+
+        # add layout for field mappings
+        self.field_mappings_layout = QVBoxLayout()
+        layout.addLayout(self.field_mappings_layout)
+        
         self.setLayout(layout)
         self.populate_note_type_combo()
         self.populate_deck_combo()
@@ -46,13 +56,40 @@ class ConfigureTableImportDialog(QDialog):
     def note_type_selected(self):
         note_type = self.note_type_combo.currentText()
         self.model.note_type_name = note_type
-        fields = self.anki_utils.get_field_list_for_note_type(note_type)
-        self.field_combo.clear()
-        self.field_combo.addItems(fields)
+        self.note_type_fields = self.anki_utils.get_field_list_for_note_type(note_type)
+        # clear all widgets from the self.field_mappings_layout layout
+        for i in reversed(range(self.field_mappings_layout.count())):
+            self.field_mappings_layout.itemAt(i).widget().setParent(None)
+        # for each field in self.note_type_fields, add a combo box populated with self.csv_field_names
+        for field in self.note_type_fields:
+            label = QLabel(field)
+            combo = QComboBox()
+            combo.setObjectName(field)
+            combo.addItems(self.all_field_names)
+            if field in self.model.field_mapping:
+                mapped_csv_field_name = self.model.field_mapping[field]
+                if mapped_csv_field_name in self.csv_field_names:
+                    combo.setCurrentText(mapped_csv_field_name)
+            self.field_mappings_layout.addWidget(label)
+            self.field_mappings_layout.addWidget(combo)
+            # when the combo box is changed, call a lambda function which contains field
+            # this will capture the current value of field in the lambda function
+            combo.currentIndexChanged.connect(lambda field=field: self.field_mapping_selected(field, combo))
+
 
     def deck_selected(self):
         deck_name = self.deck_combo.currentText()
         self.model.deck_name = deck_name
+
+    def field_mapping_selected(self, field, combo):
+        # update the model with the new field mapping
+        csv_field_name = combo.currentText()
+        if csv_field_name == self.UNMAPPED_FIELD_NAME:
+            if field in self.model.field_mapping:
+                del self.model.field_mapping[field]
+        else:
+            self.model.field_mapping[field] = csv_field_name
+
 
     
 def display_table_import_dialog_get_table_import_config(model: data.TableImportConfig, parent=None) -> data.TableImportConfig:
--- test_gui.py
+++ test_gui.py
@@ -19,12 +19,13 @@ class TestAnkiUtils():
     def get_deck_list(self) -> List[str]:
         return ['Italian', 'Mandarin', 'Cantonese']
 
-def test_batch_dialog_editor_template_error(qtbot):
+def test_configure_table_import_dialog(qtbot):
     
     table_import_config = addon.data.TableImportConfig()
     anki_utils = TestAnkiUtils()
 
-    dialog = addon.gui.ConfigureTableImportDialog(table_import_config, anki_utils)
+    csv_fieldnames = ['Chinese', 'Jyutping', 'English']
+    dialog = addon.gui.ConfigureTableImportDialog(table_import_config, csv_fieldnames, anki_utils)
 
     # assert that dialog.note_type_combo contains the note types Simple, Chinese-Words
     all_note_types = [dialog.note_type_combo.itemText(i) for i in range(dialog.note_type_combo.count())]
@@ -34,5 +35,16 @@ def test_batch_dialog_editor_template_error(qtbot):
     all_decks = [dialog.deck_combo.itemText(i) for i in range(dialog.deck_combo.count())]
     assert all_decks == ['Italian', 'Mandarin', 'Cantonese']
 
+def test_configure_table_import_dialog_choose_note_type(qtbot):
+    
+    table_import_config = addon.data.TableImportConfig()
+    anki_utils = TestAnkiUtils()
 
-    # dialog.exec()
+    csv_fieldnames = ['Chinese', 'Jyutping', 'English']
+    dialog = addon.gui.ConfigureTableImportDialog(table_import_config, csv_fieldnames, anki_utils)
+
+    dialog.note_type_combo.setCurrentText('Chinese-Words')
+    assert dialog.model.note_type_name == 'Chinese-Words'
+
+    dialog.note_type_combo.setCurrentText('Simple')
+    assert dialog.model.note_type_name == 'Simple'
