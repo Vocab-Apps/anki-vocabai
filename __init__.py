a7f74d67f8d77dc2bc679fbd81b5f65e7412d2db
Luc W <lucw@airpost.net>
2023-06-11 12:40:12 +0800

    configure pytest mode

--- __init__.py
+++ __init__.py
@@ -5,6 +5,10 @@ addon_dir = os.path.dirname(os.path.realpath(__file__))
 external_dir = os.path.join(addon_dir, 'external')
 sys.path.insert(0, external_dir)
 
-from . import addon
-
-addon.initialize()
+if hasattr(sys, '_pytest_mode'):
+    # don't do anything
+    pass 
+else:
+    # initialize anki addon
+    from . import addon
+    addon.initialize()
